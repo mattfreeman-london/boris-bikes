61f2fa196042a6323cc071c645f5ff1bcde4c0b2
Student <student@Makerss-MacBook-Pro.local>
2019-10-17 15:15:40 +0100

    clearing everything up

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,7 +3,6 @@ require 'docking_station'
 describe DockingStation do
 
   it { is_expected.to respond_to :release_bike }
-<<<<<<< HEAD
 
   it { is_expected.to respond_to :dock_bike }
 
@@ -19,11 +18,9 @@ describe DockingStation do
     expect(docking_station.bikes_docked.length).to eq 1
   end
 
-=======
-  
   it 'releases working bikes' do
     bike = subject.release_bike
     expect(bike).to be_working
   end
->>>>>>> 9adbeb6f48c75d9b9d07ffe0daf1bf0acf509115
+
 end
